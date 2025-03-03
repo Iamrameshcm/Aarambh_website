b6a727fc2640af7836fcfae1e5e5da527fa8b74b
Iamrameshcm <iamrameshcm@gmail.com>
2025-03-03 21:39:54 +0530

    added-about-us-and-minimal-change

--- src/app/hero/hero.component.ts
+++ src/app/hero/hero.component.ts
@@ -5,13 +5,26 @@ import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common'; 
 import { MatExpansionModule } from '@angular/material/expansion';
 import { MatCardModule} from '@angular/material/card'
+import { AboutUsComponent } from '../about-us/about-us.component';
+import { initializeApp } from 'firebase/app';
+import { getFirestore, collection, addDoc } from 'firebase/firestore';
 
 
-
+const firebaseConfig = {
+  apiKey: "AIzaSyBlCnOggMygMKV6nWPGC77h0C42y2DUdyg",
+  authDomain: "aarambh-new-web.firebaseapp.com",
+  projectId: "aarambh-new-web",
+  storageBucket: "aarambh-new-web.firebasestorage.app",
+  messagingSenderId: "792438087097",
+  appId: "1:792438087097:web:988ae31e908df57c42aa68",
+  measurementId: "G-26QNW8DFEL"
+};
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
 @Component({
   selector: 'app-hero',
   standalone: true,
-  imports: [MatButton,MatIcon,FormsModule,CommonModule,MatExpansionModule,MatCardModule],
+  imports: [MatButton,MatIcon,FormsModule,CommonModule,MatExpansionModule,MatCardModule,AboutUsComponent],
   templateUrl: './hero.component.html',
   styleUrl: './hero.component.scss'
 })
@@ -26,11 +39,16 @@ export class HeroComponent {
     message: ''
   };
 
-  onSubmit() {
+  async onSubmit() {
     if (this.formData.name && this.formData.phone) {
-      console.log('Form submitted:', this.formData);
-      alert('Thank you for contacting us! We will get back to you soon.');
-      this.resetForm();
+      try {
+        await addDoc(collection(db, 'contactForms'), this.formData);
+        alert('Thank you for contacting us! We will get back to you soon.');
+        this.resetForm();
+      } catch (error) {
+        console.error('Error submitting form:', error);
+        alert('Error submitting form. Please try again.');
+      }
     } else {
       alert('Please fill out the required fields.');
     }
